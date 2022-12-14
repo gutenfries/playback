bcd45d03d80b13dcf788a1f06e0f6cd47fc7183a
Mark Gutenberger <gutenfries@gmail.com>
2022-12-13 20:10:44 -0600

    format c files

--- example/macos/Runner/bridge_generated.h
+++ example/macos/Runner/bridge_generated.h
@@ -1,16 +1,16 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
-typedef struct _Dart_Handle* Dart_Handle;
+typedef struct _Dart_Handle *Dart_Handle;
 
 typedef int64_t DartPort;
 
 typedef bool (*DartPostCObjectFnType)(DartPort port_id, void *message);
 
 typedef struct WireSyncReturnStruct {
-  uint8_t *ptr;
-  int32_t len;
-  bool success;
+	uint8_t *ptr;
+	int32_t len;
+	bool success;
 } WireSyncReturnStruct;
 
 void store_dart_post_cobject(DartPostCObjectFnType ptr);
@@ -34,15 +34,15 @@ void wire_divide(int64_t port_, int32_t a, int32_t b);
 void free_WireSyncReturnStruct(struct WireSyncReturnStruct val);
 
 static int64_t dummy_method_to_enforce_bundling(void) {
-    int64_t dummy_var = 0;
-    dummy_var ^= ((int64_t) (void*) wire_platform);
-    dummy_var ^= ((int64_t) (void*) wire_rust_release_mode);
-    dummy_var ^= ((int64_t) (void*) wire_add);
-    dummy_var ^= ((int64_t) (void*) wire_divide);
-    dummy_var ^= ((int64_t) (void*) free_WireSyncReturnStruct);
-    dummy_var ^= ((int64_t) (void*) store_dart_post_cobject);
-    dummy_var ^= ((int64_t) (void*) get_dart_object);
-    dummy_var ^= ((int64_t) (void*) drop_dart_object);
-    dummy_var ^= ((int64_t) (void*) new_dart_opaque);
-    return dummy_var;
+	int64_t dummy_var = 0;
+	dummy_var ^= ((int64_t)(void *)wire_platform);
+	dummy_var ^= ((int64_t)(void *)wire_rust_release_mode);
+	dummy_var ^= ((int64_t)(void *)wire_add);
+	dummy_var ^= ((int64_t)(void *)wire_divide);
+	dummy_var ^= ((int64_t)(void *)free_WireSyncReturnStruct);
+	dummy_var ^= ((int64_t)(void *)store_dart_post_cobject);
+	dummy_var ^= ((int64_t)(void *)get_dart_object);
+	dummy_var ^= ((int64_t)(void *)drop_dart_object);
+	dummy_var ^= ((int64_t)(void *)new_dart_opaque);
+	return dummy_var;
 }
